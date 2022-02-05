c34a51a52128f20eb962088140607c554fdd5a68
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-02-05 11:56:44 +0300

    ilk örnek

---
+++ console_örnek1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodluyoruzodev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("İsminizi giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+
+
+            Console.ReadKey();
+        }
+    }
+}
